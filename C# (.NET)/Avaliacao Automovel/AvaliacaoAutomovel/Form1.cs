fe479f6590d58be9c522085548c757b0f2b217ce
José Carneiro <1232094@isep.ipp.pt>
2024-12-10 11:08:20 +0000

    Revert "teste"
    This reverts commit 384a7644b3b0290323a3dea991360cd744cd4b5b.

--- C# (.NET)/Avaliacao Automovel/AvaliacaoAutomovel/Form1.cs
+++ C# (.NET)/Avaliacao Automovel/AvaliacaoAutomovel/Form1.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Fors;
+using System.Windows.Forms;
 
 namespace AvaliacaoAutomovel
 {
